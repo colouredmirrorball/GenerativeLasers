d89d3ded2bbd78e7397cd5092ec64e986e1bb2a3
cmb <florianbonte02@gmail.com>
2020-08-05 22:07:14 +0200

    Fix memory leak in LSX output (possibly maybe)
    Replace bunch of constant ints by enum in CRectangle (probably love)

--- src/be/generativelasers/output/LsxOscOutput.java
+++ src/be/generativelasers/output/LsxOscOutput.java
@@ -1,8 +1,5 @@
 package be.generativelasers.output;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-
 import ilda.IldaFrame;
 import ilda.IldaPoint;
 import netP5.NetAddress;
@@ -10,19 +7,22 @@ import oscP5.OscMessage;
 import oscP5.OscP5;
 import processing.core.PApplet;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 import static processing.core.PApplet.constrain;
 import static processing.core.PApplet.map;
 
 /**
- * @author Florian
- * Created on 27/01/2020
+ * @author Florian Created on 27/01/2020
  */
 public class LsxOscOutput extends LaserOutput
 {
+    private final ByteBuffer b;
+    private final OscP5 osc;
     private int timeline;
     private int destinationFrame;
     private NetAddress destination;
-    private final OscP5 osc;
     private int fps = 30;
 
     public LsxOscOutput(PApplet parent, int timeline, int destinationFrame, NetAddress destination)
@@ -31,6 +31,8 @@ public class LsxOscOutput extends LaserOutput
         this.timeline = timeline;
         this.destinationFrame = destinationFrame;
         this.destination = destination;
+        b = ByteBuffer.allocate(45068); //largest point count LSX can handle is 4096
+        b.order(ByteOrder.LITTLE_ENDIAN);
     }
 
     @Override
@@ -40,26 +42,25 @@ public class LsxOscOutput extends LaserOutput
         OscMessage m = new OscMessage("/LSX_0/Frame");
 
         int pointCount = frame.getPoints().size();
-        ByteBuffer b = ByteBuffer.allocate(12+11*pointCount);
-        b.order(ByteOrder.LITTLE_ENDIAN);
+        b.position(0);
 
         // LSX frame OSC message
         //HEADER
 
-        b.put((byte) 1);         //type: 0=XYRGB; 1=XYZRGB; 2=XYZPPrRGB
+        b.put((byte) 2);         //type: 0=XYRGB; 1=XYZRGB; 2=XYZPPrRGB
         b.put((byte) 1);         //store: 0 = buffer, 1 = store in frame
         b.put((byte) timeline);  //scanner/timeline
         b.put((byte) 0);         //future
 
-        b.putShort((short)destinationFrame);
+        b.putShort((short) destinationFrame);
         b.putShort((short) pointCount);
-        b.putShort((short)0); //start point
+        b.putShort((short) 0); //start point
         b.putShort((short) pointCount);
 
         int max = 32767;
-        for (IldaPoint p : frame.getPoints())
+        for (int i = 0; i < Math.min(pointCount, 4096); i++)
         {
-
+            IldaPoint p = frame.getPoints().get(i);
             short x = (short) constrain(map(p.getPosition().x, -1, 1, -max, max), -max, max);
             b.putShort(x);
 
@@ -75,17 +76,17 @@ public class LsxOscOutput extends LaserOutput
             //    First bit: normal vector    1 = regular point    0 = normal vector
             //    Second bit: blanking        1 = blanked          0 = unblanked
             //    Third to eighth bit: palette idx (0-63)
-            //b.put((byte) (1<<7 |(p.isBlanked() ?  1 << 6 :  0)));
+            b.put((byte) (1 << 7 | (p.isBlanked() ? 1 << 6 : 0)));
 
             // Parts-Repeats byte
             //    First to fourth bit: parts (0-15)
             //    Fifth to eighth bit: repeats (0-15)
-            //b.put((byte)0);
+            b.put((byte) 0);
 
 
             int red = (p.getColour() >> 16) & 0xFF;
             int green = (p.getColour() >> 8) & 0xFF;
-            int blue =  (p.getColour() & 0xFF);
+            int blue = (p.getColour() & 0xFF);
 
             if (p.isBlanked())
             {
@@ -102,39 +103,46 @@ public class LsxOscOutput extends LaserOutput
         //Add the blob to the OSC message
         m.add(b.array());
 
-        osc.send(m,
-            destination);              // send the OSC message to the remote location defined in setup()
+        osc.send(m, destination);              // send the OSC message to the remote location defined in setup()
     }
 
-    public int getTimeline() {
+    public int getTimeline()
+    {
         return timeline;
     }
 
-    public void setTimeline(int timeline) {
+    public void setTimeline(int timeline)
+    {
         this.timeline = timeline;
     }
 
-    public int getDestinationFrame() {
+    public int getDestinationFrame()
+    {
         return destinationFrame;
     }
 
-    public void setDestinationFrame(int destinationFrame) {
+    public void setDestinationFrame(int destinationFrame)
+    {
         this.destinationFrame = destinationFrame;
     }
 
-    public NetAddress getDestination() {
+    public NetAddress getDestination()
+    {
         return destination;
     }
 
-    public void setDestination(NetAddress destination) {
+    public void setDestination(NetAddress destination)
+    {
         this.destination = destination;
     }
 
-    public int getFps() {
+    public int getFps()
+    {
         return fps;
     }
 
-    public void setFps(int fps) {
+    public void setFps(int fps)
+    {
         this.fps = fps;
     }
 }
