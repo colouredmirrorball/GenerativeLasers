a9f74f2dafbe18746a69cb99eb1c185616b8e1ca
Florian Bonte <florian.bonte@moonchase.be>
2024-01-08 19:45:54 +0100

    First light but it no work?

--- Lichtfestival/src/main/java/be/cmbsoft/lichtfestival/Lichtfestival.java
+++ Lichtfestival/src/main/java/be/cmbsoft/lichtfestival/Lichtfestival.java
@@ -3,19 +3,26 @@ package be.cmbsoft.lichtfestival;
 import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
 
+import be.cmbsoft.ilda.IldaRenderer;
 import processing.core.PApplet;
+import processing.core.PConstants;
+import processing.core.PGraphics;
 
 public class Lichtfestival extends PApplet
 {
 
-    private Laser leftLaser;
-    private Laser rightLaser;
+    private Laser     leftLaser;
+    private Laser     rightLaser;
+    private PGraphics leftGraphics;
+    private PGraphics rightGraphics;
+
 
     public Lichtfestival()
     {
 
         MidiDevice.Info[] midiDeviceInfo = MidiSystem.getMidiDeviceInfo();
-        for (MidiDevice.Info info: midiDeviceInfo) {
+        for (MidiDevice.Info info : midiDeviceInfo)
+        {
             println(info + ": " + info.getDescription() + " (" + info.getVendor() + " " + info.getVersion() + ")");
         }
 
@@ -32,14 +39,16 @@ public class Lichtfestival extends PApplet
     @Override
     public void settings()
     {
-        size(1200, 600);
+        size(1200, 600, P3D);
     }
 
     @Override
     public void setup()
     {
-        leftLaser = new Laser(this, "12A5FD136AFE");
-        rightLaser = new Laser(this, "456");
+        leftLaser = new Laser(this, "DE6656C57146");
+        rightLaser = new Laser(this, "12A5FD136AFE");
+        leftGraphics = createGraphics(width / 2, height, P3D);
+        rightGraphics = createGraphics(width / 2, height, P3D);
     }
 
     @Override
@@ -48,12 +57,26 @@ public class Lichtfestival extends PApplet
         background(0);
         fill(255);
         text("Left laser connected", 50, 40);
-        if (leftLaser.output.isConnected()) {
+        if (leftLaser.output.isConnected())
+        {
             fill(0, 255, 0);
-        } else {
+        }
+        else
+        {
             fill(255, 0, 0);
         }
         rect(20, 20, 20, 20);
+
+        IldaRenderer renderer = leftLaser.getRenderer();
+        renderer.beginDraw();
+        renderer.colorMode(PConstants.HSB);
+        renderer.stroke(frameCount % 255, 255, 255);
+        renderer.ellipse(constrain(mouseX, 0, width / 2), mouseY, 50, 50);
+        renderer.endDraw();
+
+        renderer.getCurrentFrame().renderFrame(leftGraphics, true);
+        image(leftGraphics, 0, 0, width / 2, height);
+
         leftLaser.output();
         rightLaser.output();
     }
