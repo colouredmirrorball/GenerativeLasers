2dc3382dc85ad0afa6165a72d7f350f1093aea3a
Florian Bonte <florian.bonte@moonchase.be>
2024-01-15 00:34:55 +0100

    Playing around with the new optimisation settings!

--- Lichtfestival/src/main/java/be/cmbsoft/lichtfestival/Lichtfestival.java
+++ Lichtfestival/src/main/java/be/cmbsoft/lichtfestival/Lichtfestival.java
@@ -7,6 +7,7 @@ import be.cmbsoft.ilda.IldaRenderer;
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PGraphics;
+import processing.core.PVector;
 import processing.event.KeyEvent;
 
 import static be.cmbsoft.laseroutput.OutputOption.INVERT_Y;
@@ -19,6 +20,10 @@ public class Lichtfestival extends PApplet
     private PGraphics leftGraphics;
     private PGraphics rightGraphics;
     private int       dwellAmount = 6;
+
+    PVector[] positions;
+    PVector[] ends;
+    int[]     colours;
 
 
     public Lichtfestival()
@@ -53,6 +58,18 @@ public class Lichtfestival extends PApplet
         rightLaser = new Laser(this, "12A5FD136AFE");
         leftGraphics = createGraphics(width / 2, height, P3D);
         rightGraphics = createGraphics(width / 2, height, P3D);
+
+        int amt = 10;
+        positions = new PVector[amt];
+        ends = new PVector[amt];
+        colours = new int[amt];
+
+        for (int i = 0; i < amt; i++)
+        {
+            positions[i] = new PVector(random(width), random(height));
+            ends[i] = new PVector(random(width), random(height));
+            colours[i] = color(random(255), random(255), random(255));
+        }
     }
 
     @Override
@@ -60,8 +77,33 @@ public class Lichtfestival extends PApplet
     {
         background(0);
         fill(255);
-        text("Left laser connected", 50, 40);
-        if (leftLaser.output.isConnected())
+
+
+        IldaRenderer renderer = leftLaser.getRenderer();
+        renderer.setOptimise(true);
+        renderer.getOptimisationSettings().setBlankDwellAmount(dwellAmount);
+        renderer.beginDraw();
+        renderer.colorMode(PConstants.HSB);
+        renderer.stroke(frameCount % 255, 255, 255);
+        renderer.translate(width / 2, height / 2);
+        renderer.rotate(map(mouseX, 0, width, 0, TWO_PI));
+        renderer.translate(-width / 2, -height / 2);
+        for (int i = 0; i < positions.length; i++)
+        {
+            renderer.stroke(colours[i]);
+            renderer.line(positions[i].x, positions[i].y, ends[i].x, ends[i].y);
+        }
+
+        renderer.endDraw();
+        int leftPointCount = renderer.getCurrentPointCount();
+
+        renderer.getCurrentFrame().renderFrame(leftGraphics, true, width / 2, height);
+        image(leftGraphics, 0, 0, width / 2, height);
+
+        boolean leftConnected = leftLaser.output.isConnected();
+        text("Left laser " + (leftConnected ? "" : "dis") + "connected, " + leftPointCount + " points rendered", 50,
+            40);
+        if (leftConnected)
         {
             fill(0, 255, 0);
         }
@@ -70,21 +112,6 @@ public class Lichtfestival extends PApplet
             fill(255, 0, 0);
         }
         rect(20, 20, 20, 20);
-
-        IldaRenderer renderer = leftLaser.getRenderer();
-        renderer.setOptimise(true);
-        renderer.getOptimisationSettings().setBlankDwellAmount(dwellAmount);
-        renderer.beginDraw();
-        renderer.colorMode(PConstants.HSB);
-        renderer.stroke(frameCount % 255, 255, 255);
-        if (mousePressed)
-        {
-            renderer.ellipse(constrain(mouseX, 0, width / 2), mouseY, 50, 50);
-        }
-        renderer.endDraw();
-
-        renderer.getCurrentFrame().renderFrame(leftGraphics, true, width / 2, height);
-        image(leftGraphics, 0, 0, width / 2, height);
 
         leftLaser.output();
         rightLaser.output();
